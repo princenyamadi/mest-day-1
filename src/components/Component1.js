a9f95598bd5e3e74f9bc67b452c379f193d3c2fa
princenyamadi <princenyamadi.pn@gmail.com>
2020-10-05 19:49:34 +0000

    [feat]: Added component no.1

---
+++ src/components/Component1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Component1(){
+    return (
+        <div>
+            <h1>Pre-MEST Cohort-2</h1>
+            <h6>This is my component...</h6>
+        </div>
+    );
+
+}
+
+export default Component1
---
+++ src/components/Component2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Component2(){
+    return (
+        <div className="comp2">
+            <img src="https://cdn.pixabay.com/photo/2020/08/14/17/13/light-bulbs-5488573_960_720.jpg" alt="light bulbs"/>
+            <p>component 2</p>
+        </div>
+
+    );
+}
+
+export default Component2;
