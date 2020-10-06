bd03b2dbb97a5bf1472963e2faf86e36c36a3d9d
princenyamadi <princenyamadi.pn@gmail.com>
2020-10-06 01:15:07 +0000

    [feat]: Added component no. 5

---
+++ src/components/Component5.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './css/component5.css';
+
+function Component5(){
+    return(
+        <div className="Component5">
+            <h2>Component 5</h2>
+            <div className="imglist">
+                <ul>
+                    <li>
+                        <img src="https://cdn.pixabay.com/photo/2020/10/01/11/41/cat-5618328_960_720.jpg" alt="cat"/>
+                    </li>
+                    <li>
+                        <img src="https://cdn.pixabay.com/photo/2020/10/01/11/41/cat-5618328_960_720.jpg" alt="cat"/>
+                    </li>
+                    <li>
+                        <img src="https://cdn.pixabay.com/photo/2020/10/01/11/41/cat-5618328_960_720.jpg" alt="cat"/>
+                    </li>
+                    <li>
+                        <img src="https://cdn.pixabay.com/photo/2020/10/01/11/41/cat-5618328_960_720.jpg" alt="cat"/>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    );
+}
+
+export default Component5;
