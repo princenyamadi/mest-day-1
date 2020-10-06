1f3ff5440a8cd6be65c817a4c17e745fa6fba369
princenyamadi <princenyamadi.pn@gmail.com>
2020-10-06 01:18:15 +0000

    [feat]: Added component no. 6

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Component1 from './components/Component1';
 import Component2 from './components/Component2';
 import Component3 from './components/Component3';
 import Component4 from './components/Component4';
+import Component5 from './components/Component5';
+import Component6 from './components/Component6';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Component2/>
       <Component3/>
       <Component4/>
+      <Component5/>
+      <Component6/>
       
     </div>
     // <div className="App">
---
+++ src/components/Component6.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './css/component6.css';
+
+function Component6(){
+    return(
+        <div className="Component6">
+            <h3>Component 6</h3>
+            <div className="container">
+                <div className="container-about-img">
+                    <img src="https://cdn.pixabay.com/photo/2020/09/23/02/01/microphone-5594702_960_720.jpg" alt="Music"/>
+                </div>
+                <div className="container-about-text">
+                   
+                    <p>
+                        <h2>About us</h2>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo necessitatibus eligendi labore quaerat dolorum. Velit id molestiae quibusdam magnam quos adipisci asperiores eaque non, reiciendis debitis ad sit blanditiis maxime.
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eaque iure voluptates doloribus, excepturi, consectetur exercitationem perspiciatis obcaecati delectus quas, beatae minima accusantium voluptatibus non laudantium ducimus expedita. Nisi, quam?
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ab nesciunt, voluptatibus error iste nam quidem sunt quia vitae quas, maiores iure perspiciatis neque odio itaque reprehenderit consectetur minus architecto.
+                        <br/> <a href="">READ MORE ..</a>
+                    </p>
+                </div>
+                
+            </div>
+
+        </div>
+    );
+}
+
+
+export default Component6;
