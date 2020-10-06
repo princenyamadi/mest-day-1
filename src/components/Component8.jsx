b867a42ae073378000558ed7e6efae58be0cd971
princenyamadi <princenyamadi.pn@gmail.com>
2020-10-06 02:16:43 +0000

    Added Component no. 8

---
+++ src/components/Component8.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './css/component8.css';
+
+function Component8(){
+    return(
+        <div className="Component8">
+            <ul>
+                <li>LOREM</li>
+                <li>LOREM</li>
+                <li>LOREM</li>
+                <li>LOREM</li>
+                <li>LOREM</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Component8;
